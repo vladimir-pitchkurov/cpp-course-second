75559df6e33569317486ab1082b9423fe96fda97
vladimir-pitchkurov <vladimir.pitchkurov@gmail.com>
2020-07-26 10:45:07 +0300

    extending in cpp

--- main.cpp
+++ main.cpp
@@ -100,52 +100,117 @@ int str_contains_str(const char *text, const char *pattern) {
     return -1;
 }
 
+struct Expression {
+    virtual double evaluate() const = 0;
+
+    virtual ~Expression() {}
+};
+
+struct Number : Expression {
+    Number(double value)
+            : value(value) {}
+
+    double evaluate() const {
+        return value;
+    }
+
+private:
+    const double value;
+};
+
+struct BinaryOperation : Expression {
+    /*
+      Здесь op это один из 4 символов: '+', '-', '*' или '/', соответствующих операциям,
+      которые вам нужно реализовать.
+     */
+    BinaryOperation(Expression const *left, char op, Expression const *right)
+            : left(left), op(op), right(right) {}
+
+    double evaluate() const {
+        double a = left->evaluate();
+        double b = right->evaluate();
+        switch (op) {
+            case '+':
+                return a + b;
+                break;
+            case '-':
+                return a - b;
+                break;
+            case '*':
+                return a * b;
+                break;
+            case '/':
+                return a / b;
+                break;
+        }
+    }
+
+    ~BinaryOperation() {
+        delete left;
+        delete right;
+    }
+
+private:
+    Expression const *left;
+    Expression const *right;
+    char op;
+};
+
+
 int main() {
     SetConsoleOutputCP(CP_UTF8);
 
-    struct test {
-        int ret_value;
-        const char *text;
-        const char *pattern;
-    };
-    test tests[] = {
-            {0,  "",       ""}, //0
-            {0,  "a",      ""}, //1
-            {0,  "a",      "a"}, //2
-            {-1, "a",      "b"}, //3
-            {0,  "aa",     ""}, //4
-            {0,  "aa",     "a"}, //5
-            {0,  "ab",     "a"}, //6
-            {1,  "ba",     "a"}, //7
-            {-1, "bb",     "a"}, //8
-            {0,  "aaa",    ""}, //9
-            {0,  "aaa",    "a"}, //10
-            {1,  "abc",    "b"}, //11
-            {2,  "abc",    "c"}, //12
-            {-1, "abc",    "d"}, //13
-            {-1, "a",      "aa"}, //14
-            {-1, "a",      "ba"}, //15
-            {-1, "a",      "ab"}, //16
-            {-1, "a",      "bb"}, //17
-            {-1, "a",      "aaa"}, //18
-            {-1, "aa",     "aaa"}, //19
-            {0,  "aaa",    "aaa"}, //20
-            {0,  "aaab",   "aaa"}, //21
-            {1,  "baaa",   "aaa"}, //22
-            {1,  "baaaa",  "aaa"}, //23
-            {1,  "baaab",  "aaa"}, //24
-            {-1, "abd",    "abc"}, //25
-            {2,  "ababc",  "abc"}, //26
-            {3,  "abdabc", "abc"}, //27
-            {-1, "",       "a"}, //28
-            {2,  "asasaf", "asaf"}, //29
-            {2,  "ababac", "abac"} //30
-    };
-    for (int i = 0; i < sizeof(tests) / sizeof(tests[0]); ++i) {
-        int ret = str_contains_str(tests[i].text, tests[i].pattern);
-        (tests[i].ret_value == ret) ? cout << "OK" : cout << "Failed";
-        cout << " : " << i << " (" << tests[i].ret_value << " : " << ret << ")" << endl;
-    }
+    Expression *ex1 = new Number(3.0);
+    Expression *ex2 = new Number(2.0);
+
+    Expression *sub_ab = new BinaryOperation(ex1, '-', ex2);
+    std::cout << sub_ab->evaluate() << std::endl;
+
+    delete sub_ab; // уничтожили a и b
+
+//    struct test {
+//        int ret_value;
+//        const char *text;
+//        const char *pattern;
+//    };
+//    test tests[] = {
+//            {0,  "",       ""}, //0
+//            {0,  "a",      ""}, //1
+//            {0,  "a",      "a"}, //2
+//            {-1, "a",      "b"}, //3
+//            {0,  "aa",     ""}, //4
+//            {0,  "aa",     "a"}, //5
+//            {0,  "ab",     "a"}, //6
+//            {1,  "ba",     "a"}, //7
+//            {-1, "bb",     "a"}, //8
+//            {0,  "aaa",    ""}, //9
+//            {0,  "aaa",    "a"}, //10
+//            {1,  "abc",    "b"}, //11
+//            {2,  "abc",    "c"}, //12
+//            {-1, "abc",    "d"}, //13
+//            {-1, "a",      "aa"}, //14
+//            {-1, "a",      "ba"}, //15
+//            {-1, "a",      "ab"}, //16
+//            {-1, "a",      "bb"}, //17
+//            {-1, "a",      "aaa"}, //18
+//            {-1, "aa",     "aaa"}, //19
+//            {0,  "aaa",    "aaa"}, //20
+//            {0,  "aaab",   "aaa"}, //21
+//            {1,  "baaa",   "aaa"}, //22
+//            {1,  "baaaa",  "aaa"}, //23
+//            {1,  "baaab",  "aaa"}, //24
+//            {-1, "abd",    "abc"}, //25
+//            {2,  "ababc",  "abc"}, //26
+//            {3,  "abdabc", "abc"}, //27
+//            {-1, "",       "a"}, //28
+//            {2,  "asasaf", "asaf"}, //29
+//            {2,  "ababac", "abac"} //30
+//    };
+//    for (int i = 0; i < sizeof(tests) / sizeof(tests[0]); ++i) {
+//        int ret = str_contains_str(tests[i].text, tests[i].pattern);
+//        (tests[i].ret_value == ret) ? cout << "OK" : cout << "Failed";
+//        cout << " : " << i << " (" << tests[i].ret_value << " : " << ret << ")" << endl;
+//    }
 
     return 0;
 }
